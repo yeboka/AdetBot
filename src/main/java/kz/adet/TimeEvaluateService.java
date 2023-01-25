6dab91ee641209f04a5835992bb597ab34f841ae
Yeboka <89529214+Yeboka@users.noreply.github.com>
2023-01-25 14:58:26 +0600

    refactor code

--- src/main/java/kz/adet/TimeEvaluateService.java
+++ src/main/java/kz/adet/TimeEvaluateService.java
@@ -1,22 +1,22 @@
 package kz.adet;
 
-//FIXME write tests for this class
+//TODO write tests for this class
 public class TimeEvaluateService {
 
     private final int needTimeHour;
     private final int needTimeMinute;
 
-    TimeEvaluateService (int needTimeHour, int needTimeMinute) {
+    public TimeEvaluateService (int needTimeHour, int needTimeMinute) {
         this.needTimeHour = needTimeHour;
         this.needTimeMinute = needTimeMinute;
     }
 
-    public long getMillisToSleep (long currTimeInMillis) {
+    public long getInitialDelay (long currTimeInMillis) {
         long needTimeInMillis = (long) needTimeHour * 60 * 60 * 1000 + (long) needTimeMinute * 60 * 1000;
         long diff = Math.abs(needTimeInMillis - currTimeInMillis);
 
         if (currTimeInMillis > needTimeInMillis)
-            return 24 * 60 * 60 * 1000 - diff;            //from all millis in a day subtract difference
+            return (24 * 60 * 60 * 1000 - diff);            //from all millis in a day subtract difference
         else
             return diff;
     }
